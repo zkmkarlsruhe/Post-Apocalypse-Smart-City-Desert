5c7a62c3f56a878dadb820812028c42974dc639e
Christian Lölkes <christian.loelkes@gmail.com>
2019-08-03 00:11:26 +0200

    Version 1.0.1: Code cleanup.

--- js/ploeger.js
+++ js/ploeger.js
@@ -2,27 +2,23 @@
 var camera, scene, renderer, controls, container, video;
 
 function init() {
-	container = document.getElementById('container');
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 1100);
   controls = new THREE.DeviceOrientationControls(camera);
 	camera.target = new THREE.Vector3(0, 0, 0);
 
 	scene = new THREE.Scene();
-
 	// Define the geomtry holding the video
 	let geometry = new THREE.SphereBufferGeometry(500, 60, 40);
 	// correctly orient the surface by scaling inverting it
 	geometry.scale(-1, 1, 1);
 
 	video = document.createElement('video');
-	video.crossOrigin = 'anonymous';
-	video.width, video.height  = 1080, 1920;
-	video.loop, video.muted = true, true;
+	video.crossOrigin 					= 'anonymous';
+	video.width, video.height  	= 1080, 1920;
+	video.loop, video.muted 		= true, true;
 	video.setAttribute('webkit-playsinline', 'webkit-playsinline');
-	video.setAttribute('playsinline','');
-
-	urlParams = getParams(location.href);
-	video.src = 'media/' + urlParams.id + '.mp4';
+	video.setAttribute('playsinline', '');
+	video.src = 'media/' + getParams(location.href).id + '.mp4';
 
 	let texture = new THREE.VideoTexture(video);
 	let material = new THREE.MeshBasicMaterial({map: texture});
@@ -31,7 +27,7 @@ function init() {
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	container.appendChild(renderer.domElement);
+	document.getElementById('container').appendChild(renderer.domElement);
 	window.addEventListener('resize', onWindowResize, false);
 	window.addEventListener('orientationchange', onWindowResize, false);
 }
